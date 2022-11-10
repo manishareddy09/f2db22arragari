02f60322dc0c8c48b5f6a796630d762a4a421c9e
manishareddy09 <111670516+manishareddy09@users.noreply.github.com>
2022-11-09 18:39:16 -0600

    Added

--- controllers/ball.js
+++ controllers/ball.js
@@ -19,8 +19,25 @@ exports.ball_detail = function(req, res) {
 }; 
  
 // Handle ball create on POST. 
-exports.ball_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: ball create POST'); 
+// Handle ball create on POST. 
+exports.ball_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new ball(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"ball_type":"goat", "cost":12, "size":"large"} 
+    document.ball_colour = req.body.ball_colour; 
+    document.ball_size = req.body.ball_size; 
+    document.ball_cost = req.body.ball_cost; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // Handle ball delete form on DELETE. 
